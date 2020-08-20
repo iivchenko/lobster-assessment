70360b19788bc9ba47131e15f9638510828606d3
iivchenko <iivchenko@live.com>
2020-08-20 13:44:23 +0200

    * Design test cases for GetQuestion Query

--- src/Story.Application/Queries/GetQuestion/GetQuestionQueryHandler.cs
+++ src/Story.Application/Queries/GetQuestion/GetQuestionQueryHandler.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
+using Story.Application.Domain.Stories;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,8 +9,24 @@ namespace Story.Application.Queries.GetQuestion
 {
     public sealed class GetQuestionQueryHandler : IRequestHandler<GetQuestionQuery, GetQuestionResponse>
     {
+        private readonly IStoryRepository _storyRepository;
+        private readonly IMapper _mapper;
+
+        public GetQuestionQueryHandler(
+            IStoryRepository storyRepository,
+            IMapper mapper)
+        {
+            _storyRepository = storyRepository;
+            _mapper = mapper;
+        }
+
         public Task<GetQuestionResponse> Handle(GetQuestionQuery request, CancellationToken cancellationToken)
         {
+            // get story by id
+            // if no story - throws
+            // find question by id 
+            // if no questio - throws
+            // map and return question
             throw new NotImplementedException();
         }
     }
