03ebc0e71761f03e7297477a74a3aa11299bd3e3
iivchenko <iivchenko@live.com>
2020-08-21 14:57:28 +0200

    [angular] Add 'Strangers path' to the story component

--- src/Story.Host/ClientApp/src/app/story/story.compenent.ts
+++ src/Story.Host/ClientApp/src/app/story/story.compenent.ts
@@ -1,6 +1,5 @@
 import { Component, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -8,10 +7,12 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './story.component.html'
 })
 export class StoryComponent {
-  public story: Story;
-  public question: Question;
-  public end: End;
+  public story: Story = { id: null, name: null, description: null, rootQuestionId: null };;
+  public question: Question = { id: null, text: null, answers: [] };
+  public end: End = { id: null, message: null };
   public finish: boolean;
+
+  public history: HistoryItem[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -26,8 +27,13 @@ export class StoryComponent {
     this.http.get<Story>(this.baseUrl + 'api/stories/' + id).subscribe(result => {
       this.story = result;
 
+      this.pushHistory(this.story.name, 1);
+
       this.http.get<Question>(this.baseUrl + 'api/stories/' + id + '/questions/' + this.story.rootQuestionId).subscribe(result => {
         this.question = result;
+
+        this.pushHistory(this.question.text, 2);
+
       }, error => console.error(error));
 
     }, error => console.error(error));
@@ -36,25 +42,42 @@ export class StoryComponent {
   giveAnswer(id: string) {
     this.http.get<Answer>(this.baseUrl + 'api/stories/' + this.story.id + '/answers/' + id).subscribe(result => {
 
+      this.pushHistory(result.text, 3);
+
       if (result.nextEntityType === 0) {
         this.http.get<Question>(this.baseUrl + 'api/stories/' + this.story.id + '/questions/' + result.nextEntityId).subscribe(result => {
           this.question = result;
+
+          this.pushHistory(this.question.text, 2);
+
         }, error => console.error(error));
       }
       else {
         this.http.get<End>(this.baseUrl + 'api/stories/' + this.story.id + '/end/' + result.nextEntityId).subscribe(result => {
           this.end = result;
+
+          this.pushHistory(this.end.message, 4);
+
         }, error => console.error(error));
         this.finish = true;
       }   
     }, error => console.error(error));
+  }
+
+  pushHistory(text, type) {
+    const item: HistoryItem = {
+      text: text,
+      type: type
+    };
+
+    this.history.push(item)
   }
 }
 
 interface Story {
   id: string;
   name: string;
-  desciption: string;
+  description: string;
   rootQuestionId: string;
 }
 
@@ -74,4 +97,9 @@ interface Answer {
 interface End {
   id: string;
   message: string;
+}
+
+interface HistoryItem {
+  text: string;
+  type: number;
 }
