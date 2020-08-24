dc7b24df52728b122f81e551704ee236ffd4886a
iivchenko <iivchenko@live.com>
2020-08-24 13:28:43 +0200

    [angular] Add ngx-graph for detailed story graph creation

--- src/Story.Host/ClientApp/src/app/app.module.ts
+++ src/Story.Host/ClientApp/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { NgxGraphModule } from '@swimlane/ngx-graph'
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -31,7 +33,9 @@ import { StoryComponent } from './story/story.compenent'
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
       { path: 'story/:id', component: StoryComponent },
-    ])
+    ]),
+    BrowserAnimationsModule,
+    NgxGraphModule
   ],
   providers: [],
   bootstrap: [AppComponent]
