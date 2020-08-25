65c026f10f0d93ca4f4742909e10a412985e3c52
iivchenko <iivchenko@live.com>
2020-08-25 09:39:19 +0200

    [angular] Implement story graph visualization

--- src/Story.Host/ClientApp/src/app/story/story.compenent.ts
+++ src/Story.Host/ClientApp/src/app/story/story.compenent.ts
@@ -1,9 +1,12 @@
 import { Component, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { Edge, Node } from '@swimlane/ngx-graph';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'story-component',
+  styleUrls: ['./story.component.css'],
   templateUrl: './story.component.html'
 })
 export class StoryComponent {
@@ -11,6 +14,16 @@ export class StoryComponent {
   public question: Question = { id: null, text: null, answers: [] };
   public end: End = { id: null, message: null };
   public finish: boolean;
+
+  public nodes: Node[] = [];
+  public links: Edge[] = [];  
+  public treeIsBuild: boolean = false;
+  public layoutSettings = {
+    orientation: 'TB'
+  };
+  public update$: Subject<boolean> = new Subject();
+  public center$: Subject<boolean> = new Subject();
+  public zoomToFit$: Subject<boolean> = new Subject();
 
   public history: HistoryItem[] = [];
 
@@ -27,12 +40,12 @@ export class StoryComponent {
     this.http.get<Story>(this.baseUrl + 'api/stories/' + id).subscribe(result => {
       this.story = result;
 
-      this.pushHistory(this.story.name, 1);
+      this.pushHistory(this.story.id, this.story.name, 1);
 
       this.http.get<Question>(this.baseUrl + 'api/stories/' + id + '/questions/' + this.story.rootQuestionId).subscribe(result => {
         this.question = result;
 
-        this.pushHistory(this.question.text, 2);
+        this.pushHistory(this.question.id, this.question.text, 2);
 
       }, error => console.error(error));
 
@@ -42,13 +55,13 @@ export class StoryComponent {
   giveAnswer(id: string) {
     this.http.get<Answer>(this.baseUrl + 'api/stories/' + this.story.id + '/answers/' + id).subscribe(result => {
 
-      this.pushHistory(result.text, 3);
+      this.pushHistory(result.id, result.text, 3);
 
       if (result.nextEntityType === 0) {
         this.http.get<Question>(this.baseUrl + 'api/stories/' + this.story.id + '/questions/' + result.nextEntityId).subscribe(result => {
           this.question = result;
 
-          this.pushHistory(this.question.text, 2);
+          this.pushHistory(this.question.id, this.question.text, 2);
 
         }, error => console.error(error));
       }
@@ -56,7 +69,7 @@ export class StoryComponent {
         this.http.get<End>(this.baseUrl + 'api/stories/' + this.story.id + '/end/' + result.nextEntityId).subscribe(result => {
           this.end = result;
 
-          this.pushHistory(this.end.message, 4);
+          this.pushHistory(this.end.id, this.end.message, 4);
 
         }, error => console.error(error));
         this.finish = true;
@@ -64,14 +77,98 @@ export class StoryComponent {
     }, error => console.error(error));
   }
 
-  pushHistory(text, type) {
+  pushHistory(id, text, type) {
     const item: HistoryItem = {
+      id: id,
       text: text,
       type: type
     };
 
     this.history.push(item)
   }
+
+  buildTree() {
+    this.http.get<FullStory>(this.baseUrl + 'api/stories/' + this.story.id + '/full').subscribe(result => {
+      this.handleQuestion(result.root);
+      this.treeIsBuild = true;
+      this.center$.next(true);
+      this.zoomToFit$.next(true);
+      this.update$.next(true);
+      
+    }, error => console.error(error));
+  }
+
+  handleQuestion(question: FullQuestion) {
+    const node: Node = {
+      id: question.id,
+      label: question.text,
+      data: {
+        selected: this.history.some(x => x.id === question.id),
+        type: 'question'
+      }
+    };
+
+    this.nodes.push(node);
+
+    for (var answer of question.answers) {
+      const link: Edge = {
+        source: question.id,
+        target: answer.id,
+      };
+
+      this.links.push(link);
+    }
+
+    for (var answer of question.answers) {
+      this.handleAnswer(answer);
+    }
+  }
+
+  handleAnswer(answer: FullAnswer) {
+    const node: Node = {
+      id: answer.id,
+      label: answer.text,
+      data: {
+        selected: this.history.some(x => x.id === answer.id),
+        type: 'answer'
+      }
+    };
+
+    this.nodes.push(node);
+
+    switch (answer.nextType) {
+      case 0:
+        const question: FullQuestion = answer.next;
+        const link1: Edge = {
+          source: answer.id,
+          target: question.id,
+        };
+        this.links.push(link1);
+
+        this.handleQuestion(question);
+
+        break;
+      case 1:
+        const end: FullEnd = answer.next;
+        const link2: Edge = {
+          source: answer.id,
+          target: end.id,
+        };
+        this.links.push(link2);
+
+        const node: Node = {
+          id: end.id,
+          label: end.message,
+          data: {
+            selected: this.history.some(x => x.id === end.id),
+            type: 'end'
+          }
+        };
+
+        this.nodes.push(node);
+        break;
+    }
+  }  
 }
 
 interface Story {
@@ -100,6 +197,31 @@ interface End {
 }
 
 interface HistoryItem {
+  id: string;
   text: string;
   type: number;
+}
+
+interface FullEnd {
+  id: string;
+  message: string;
+}
+
+interface FullAnswer {
+  id: string;
+  text: string;
+  nextType: number;
+  next: any;
+}
+
+interface FullQuestion {
+  id: string;
+  text: string;
+  answers: FullAnswer[]
+}
+
+interface FullStory {
+  id: string;
+  name: string;
+  root: FullQuestion;
 }
