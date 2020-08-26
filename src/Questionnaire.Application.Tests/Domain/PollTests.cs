1add734a4b0de805283ead1a3f444e107006d177
iivchenko <iivchenko@live.com>
2020-08-26 13:56:21 +0200

    Implement Poll

--- src/Questionnaire.Application.Tests/Domain/PollTests.cs
+++ src/Questionnaire.Application.Tests/Domain/PollTests.cs
@@ -1,81 +1,367 @@
 ﻿using NUnit.Framework;
+using Story.Application.Domain.Common;
+using Story.Application.Domain.Polls;
+using System;
+using System.Linq;
 
 namespace Questionnaire.Application.Tests.Domain
 {
     [TestFixture]
     public sealed class PollTests
     {
+        private const string ProperName = "Some Name";
+        private const string ProperDescription = "Some Description";
+        private const string ProperText = "Some Text";
 
         [Test]
         public void Create_EmptyId_Throws()
         {
-            // Arrange
-            // Assert
-            // Act
+            //Arrange+Act+Assert
+            Assert
+                .That(
+                    () => new Poll(Guid.Empty, null, null, Guid.Empty, null, null),
+                    Throws
+                        .InstanceOf<DomainException>()
+                        .With
+                        .Message
+                        .EqualTo("'id' can't be empty!"));
+        }
+
+        [Test]
+        public void Create_NameIsEmpty_Throws()
+        {
+            //Arrange+Act+Assert
+            Assert
+                .That(
+                    () => new Poll(Guid.NewGuid(), string.Empty, null, Guid.Empty, null, null),
+                    Throws
+                        .InstanceOf<DomainException>()
+                        .With
+                        .Message
+                        .EqualTo("'name' can't be empty!"));
+        }
+
+        [Test]
+        public void Create_DescriptionIsEmpty_Throws()
+        {
+            //Arrange+Act+Assert
+            Assert
+                .That(
+                    () => new Poll(Guid.NewGuid(), ProperName, string.Empty, Guid.Empty, null, null),
+                    Throws
+                        .InstanceOf<DomainException>()
+                        .With
+                        .Message
+                        .EqualTo("'description' can't be empty!"));
         }
 
         [Test]
         public void Create_EmptyRootQuestionId_Throws()
         {
-            // Arrange
-            // Assert
-            // Act
+            //Arrange+Act+Assert
+            Assert
+                .That(
+                    () => new Poll(Guid.NewGuid(), ProperName, ProperDescription, Guid.Empty, null, null),
+                    Throws
+                        .InstanceOf<DomainException>()
+                        .With
+                        .Message
+                        .EqualTo("'rootQuestionId' can't be empty!"));
         }
 
         [Test]
         public void Create_EmptyItems_Throws()
         {
-            // Arrange
-            // Assert
-            // Act
+            //Arrange+Act+Assert
+            Assert
+                .That(
+                    () => new Poll(Guid.NewGuid(), ProperName, ProperDescription, Guid.NewGuid(), Enumerable.Empty<PollItem>(), null),
+                    Throws
+                        .InstanceOf<DomainException>()
+                        .With
+                        .Message
+                        .EqualTo("'items' can't be empty!"));
         }
 
         [Test]
         public void Create_EmptyTransitions_Throws()
         {
-            // Arrange
-            // Assert
-            // Act
+            // Arrange 
+            var items = new[] { new PollEnd(Guid.NewGuid(), ProperText) };
+
+            // Act+Assert
+            Assert
+                .That(
+                    () => new Poll(Guid.NewGuid(), ProperName, ProperDescription, Guid.NewGuid(), items, Enumerable.Empty<Transition>()),
+                    Throws
+                        .InstanceOf<DomainException>()
+                        .With
+                        .Message
+                        .EqualTo("'transitions' can't be empty!"));
         }
 
         [Test]
         public void Create_RootQuestionIdNotPresentInItems_Throws()
         {
-            // Arrange
-            // Assert
-            // Act
+            // Arrange           
+            var rootQuestion = new PollQuestion(Guid.NewGuid(), ProperText);
+            var question = new PollQuestion(Guid.NewGuid(), ProperText);
+            var answer = new PollAnswer(Guid.NewGuid(), ProperText);            
+            var items = new PollItem [] { question, answer };
+            var transitions = new[] { Transition.Create(answer, question) };
+
+            // Act+Assert
+            Assert
+                .That(
+                    () => new Poll(Guid.NewGuid(), ProperName, ProperDescription, rootQuestion.Id, items, transitions),
+                    Throws
+                        .InstanceOf<DomainException>()
+                        .With
+                        .Message
+                        .EqualTo($"Root questioin (id: '{rootQuestion.Id}' is not presetn in items!"));
         }
 
         [Test]
-        public void Create_ItemsNotPresentInTransitions_Throws()
+        public void Create_RootQuestionNotPresentInTransitionis_Throws()
         {
-            // Arrange
-            // Assert
-            // Act
+            // Arrange           
+            var rootQuestion = new PollQuestion(Guid.NewGuid(), ProperText);
+            var question = new PollQuestion(Guid.NewGuid(), ProperText);
+            var answer = new PollAnswer(Guid.NewGuid(), ProperText);
+            var items = new PollItem[] { rootQuestion, question, answer };
+            var transitions = new[] { Transition.Create(answer, question) };
+
+            // Act+Assert
+            Assert
+                .That(
+                    () => new Poll(Guid.NewGuid(), ProperName, ProperDescription, rootQuestion.Id, items, transitions),
+                    Throws
+                        .InstanceOf<DomainException>()
+                        .With
+                        .Message
+                        .EqualTo($"Question (id: '{rootQuestion.Id}' should have transition!"));
         }
 
         [Test]
-        public void Create_TransitionsNotPresentInItems_Throws()
+        public void Create_QuestionNotPresentInTransitions_Throws()
         {
-            // Arrange
-            // Assert
-            // Act
+            // Arrange           
+            var rootQuestion = new PollQuestion(Guid.NewGuid(), ProperText);
+            var question = new PollQuestion(Guid.NewGuid(), ProperText);
+            var answer = new PollAnswer(Guid.NewGuid(), ProperText);
+            var items = new PollItem[] { rootQuestion, question, answer };
+            var transitions = new[] { Transition.Create(rootQuestion, answer) };
+
+            // Act+Assert
+            Assert
+                .That(
+                    () => new Poll(Guid.NewGuid(), ProperName, ProperDescription, rootQuestion.Id, items, transitions),
+                    Throws
+                        .InstanceOf<DomainException>()
+                        .With
+                        .Message
+                        .EqualTo($"Question (id: '{question.Id}' should have transition!"));
         }
 
         [Test]
-        public void Create_ItemsHaveIncorectTransitions_Throws()
+        public void Create_AnswerNotPresentInTransitions_Throws()
         {
-            // Arrange
-            // Assert
-            // Act
+            // Arrange           
+            var rootQuestion = new PollQuestion(Guid.NewGuid(), ProperText);
+            var answer = new PollAnswer(Guid.NewGuid(), ProperText);
+            var items = new PollItem[] { rootQuestion, answer };
+            var transitions = new[] { Transition.Create(rootQuestion, answer) };
+
+            // Act+Assert
+            Assert
+                .That(
+                    () => new Poll(Guid.NewGuid(), ProperName, ProperDescription, rootQuestion.Id, items, transitions),
+                    Throws
+                        .InstanceOf<DomainException>()
+                        .With
+                        .Message
+                        .EqualTo($"Answer (id: '{answer.Id}' should have transition!"));
         }
 
         [Test]
-        public void Create_AllConditionsAreMet_CreatePoll()
+        public void FindNextFor_Question_QuestionIsNull_Throws()
         {
-            // Arrange
-            // Assert
+            // Arrange           
+            var rootQuestion = new PollQuestion(Guid.NewGuid(), ProperText);
+            var answer = new PollAnswer(Guid.NewGuid(), ProperText);
+            var end = new PollEnd(Guid.NewGuid(), ProperText);
+
+            var items = new PollItem[] { rootQuestion, answer, end };
+            var transitions = new[] { Transition.Create(rootQuestion, answer), Transition.Create(answer, end) };
+
+            var poll = new Poll(Guid.NewGuid(), ProperName, ProperDescription, rootQuestion.Id, items, transitions);
+
+            // Act+Assert
+            Assert
+                .That(
+                    () => poll.FindNextFor((PollQuestion)null),
+                    Throws
+                        .InstanceOf<DomainException>()
+                        .With
+                        .Message
+                        .EqualTo($"Question should not be null!"));
+        }
+
+        [Test]
+        public void FindNextFor_Question_QuestionIsNotPresentInPoll_Throws()
+        {
+            // Arrange           
+            var rootQuestion = new PollQuestion(Guid.NewGuid(), ProperText);
+            var answer = new PollAnswer(Guid.NewGuid(), ProperText);
+            var end = new PollEnd(Guid.NewGuid(), ProperText);
+
+            var fakeQuestion = new PollQuestion(Guid.NewGuid(), ProperText);
+
+            var items = new PollItem[] { rootQuestion, answer, end };
+            var transitions = new[] { Transition.Create(rootQuestion, answer), Transition.Create(answer, end) };
+
+            var poll = new Poll(Guid.NewGuid(), ProperName, ProperDescription, rootQuestion.Id, items, transitions);
+
+            // Act+Assert
+            Assert
+                .That(
+                    () => poll.FindNextFor(fakeQuestion),
+                    Throws
+                        .InstanceOf<DomainException>()
+                        .With
+                        .Message
+                        .EqualTo($"Question (id: '{fakeQuestion.Id}' not present in the Poll!"));
+        }
+
+        [Test]
+        public void FindNextFor_Question_AllConditionsAreMet_ReturnAnswers()
+        {
+            // Arrange           
+            var rootQuestion = new PollQuestion(Guid.NewGuid(), ProperText);
+            var answer1 = new PollAnswer(Guid.NewGuid(), ProperText);
+            var answer2 = new PollAnswer(Guid.NewGuid(), ProperText);
+            var end = new PollEnd(Guid.NewGuid(), ProperText);
+
+            var items = new PollItem[] { rootQuestion, answer1, answer2, end };
+            var transitions = new[] 
+                { 
+                    Transition.Create(rootQuestion, answer1),
+                    Transition.Create(rootQuestion, answer2),
+                    Transition.Create(answer1, end),
+                    Transition.Create(answer2, end)
+                };
+
+            var poll = new Poll(Guid.NewGuid(), ProperName, ProperDescription, rootQuestion.Id, items, transitions);
+
             // Act
+            var actualAnswers = poll.FindNextFor(rootQuestion);
+
+            // Assert
+            Assert.That(actualAnswers.Count(), Is.EqualTo(2));
+            Assert.That(actualAnswers, Contains.Item(answer1));
+            Assert.That(actualAnswers, Contains.Item(answer2));
+        }
+
+        [Test]
+        public void FindNextFor_Answer_AnsswerIsNull_Throws()
+        {
+            // Arrange           
+            var rootQuestion = new PollQuestion(Guid.NewGuid(), ProperText);
+            var answer = new PollAnswer(Guid.NewGuid(), ProperText);
+            var end = new PollEnd(Guid.NewGuid(), ProperText);
+
+            var items = new PollItem[] { rootQuestion, answer, end };
+            var transitions = new[] { Transition.Create(rootQuestion, answer), Transition.Create(answer, end) };
+
+            var poll = new Poll(Guid.NewGuid(), ProperName, ProperDescription, rootQuestion.Id, items, transitions);
+
+            // Act+Assert
+            Assert
+                .That(
+                    () => poll.FindNextFor((PollAnswer)null),
+                    Throws
+                        .InstanceOf<DomainException>()
+                        .With
+                        .Message
+                        .EqualTo($"Answer should not be null!"));
+        }
+
+        [Test]
+        public void FindNextFor_Answer_AnswerIsNotPresentInPoll_Throws()
+        {
+            // Arrange           
+            var rootQuestion = new PollQuestion(Guid.NewGuid(), ProperText);
+            var answer = new PollAnswer(Guid.NewGuid(), ProperText);
+            var end = new PollEnd(Guid.NewGuid(), ProperText);
+
+            var fakeAnswer = new PollAnswer(Guid.NewGuid(), ProperText);
+
+            var items = new PollItem[] { rootQuestion, answer, end };
+            var transitions = new[] { Transition.Create(rootQuestion, answer), Transition.Create(answer, end) };
+
+            var poll = new Poll(Guid.NewGuid(), ProperName, ProperDescription, rootQuestion.Id, items, transitions);
+
+            // Act+Assert
+            Assert
+                .That(
+                    () => poll.FindNextFor(fakeAnswer),
+                    Throws
+                        .InstanceOf<DomainException>()
+                        .With
+                        .Message
+                        .EqualTo($"Answer (id: '{fakeAnswer.Id}' not present in the Poll!"));
+        }
+
+        [Test]
+        public void FindNextFor_Answer_AllConditionsAreMet_ReturnEnd()
+        {
+            // Arrange           
+            var rootQuestion = new PollQuestion(Guid.NewGuid(), ProperText);
+            var answer = new PollAnswer(Guid.NewGuid(), ProperText);
+            var end = new PollEnd(Guid.NewGuid(), ProperText);
+
+            var items = new PollItem[] { rootQuestion, answer, end };
+            var transitions = new[]
+                {
+                    Transition.Create(rootQuestion, answer),
+                    Transition.Create(answer, end)
+                };
+
+            var poll = new Poll(Guid.NewGuid(), ProperName, ProperDescription, rootQuestion.Id, items, transitions);
+
+            // Act
+            var actualEnd = poll.FindNextFor(answer);
+
+            // Assert
+            Assert.That(actualEnd, Is.EqualTo(end));
+        }
+
+        [Test]
+        public void FindNextFor_Answer_AllConditionsAreMet_ReturnQuestion()
+        {
+            // Arrange           
+            var rootQuestion = new PollQuestion(Guid.NewGuid(), ProperText);
+            var answer1 = new PollAnswer(Guid.NewGuid(), ProperText);
+            var question = new PollQuestion(Guid.NewGuid(), ProperText);
+            var answer2 = new PollAnswer(Guid.NewGuid(), ProperText);
+            var end = new PollEnd(Guid.NewGuid(), ProperText);
+
+            var items = new PollItem[] { rootQuestion, answer1, question, answer2, end };
+            var transitions = new[]
+                {
+                    Transition.Create(rootQuestion, answer1),
+                    Transition.Create(answer1, question),
+                    Transition.Create(question, answer2),
+                    Transition.Create(answer2, end)
+                };
+
+            var poll = new Poll(Guid.NewGuid(), ProperName, ProperDescription, rootQuestion.Id, items, transitions);
+
+            // Act
+            var actualQuestion = poll.FindNextFor(answer1);
+
+            // Assert
+            Assert.That(actualQuestion, Is.EqualTo(question));
         }
     }
 }
--- src/Story.Application/Domain/Polls/Poll.cs
+++ src/Story.Application/Domain/Polls/Poll.cs
@@ -1,19 +1,75 @@
 ﻿using Story.Application.Domain.Common;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace Story.Application.Domain.Polls
 {
     public sealed class Poll : IAggregateRoot<Guid>
     {
-        private readonly IEnumerable<PollItem> _items;
-        private readonly IEnumerable<Transition> _transitions;
+        [JsonProperty] private readonly IEnumerable<PollItem> _items;
+        [JsonProperty] private readonly IEnumerable<Transition> _transitions;
 
-        public Poll(Guid id, Guid rootQuestionId, IEnumerable<PollItem> items, IEnumerable<Transition> transitions)
+        public Poll(Guid id, string name, string description, Guid rootQuestionId, IEnumerable<PollItem> items, IEnumerable<Transition> transitions)
         {
-            // check items has rootQuestionId
-            // check all items used property in transtitions
-            // check transition has correct directions
+            if (id == Guid.Empty)
+            {
+                throw new DomainException($"'{nameof(id)}' can't be empty!");
+            }
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new DomainException($"'{nameof(name)}' can't be empty!");
+            }
+
+            if (string.IsNullOrWhiteSpace(description))
+            {
+                throw new DomainException($"'{nameof(description)}' can't be empty!");
+            }
+
+            if (rootQuestionId == Guid.Empty)
+            {
+                throw new DomainException($"'{nameof(rootQuestionId)}' can't be empty!");
+            }
+
+            if (items == null || !items.Any())
+            {
+                throw new DomainException($"'{nameof(items)}' can't be empty!");
+            }
+
+            if (transitions == null || !transitions.Any())
+            {
+                throw new DomainException($"'{nameof(transitions)}' can't be empty!");
+            }
+
+            if (!items.Any(x => x.Id == rootQuestionId))
+            {
+                throw new DomainException($"Root questioin (id: '{rootQuestionId}' is not presetn in items!");
+            }
+
+            ValidateQuestionTransition(rootQuestionId, items, transitions);
+
+            foreach(var item in items)
+            {
+                switch(item)
+                {
+                    case PollQuestion question:
+                        ValidateQuestionTransition(question.Id, items, transitions);
+                        break;
+                    case PollAnswer answer:
+                        ValidateAnswerTransition(answer.Id, items, transitions);
+                        break;
+                }
+            }
+
+            Id = id;
+            Name = name;
+            Description = description;
+            RootQuestion = (PollQuestion)items.Single(x => x.Id == rootQuestionId);
+
+            _items = items;
+            _transitions = transitions;
         }
 
         public Guid Id { get; private set; }
@@ -26,16 +82,63 @@ namespace Story.Application.Domain.Polls
 
         public IEnumerable<PollAnswer> FindNextFor(PollQuestion question)
         {
-            // check quesiton exists in items if not return null
-            // check find transition and return
-            return null;
+            if (question == null)
+            {
+                throw new DomainException($"Question should not be null!");
+            }
+
+            if (!_items.Any(x => x.Id == question.Id))
+            {
+                throw new DomainException($"Question (id: '{question.Id}' not present in the Poll!");
+            }
+
+            var nextItemIds = 
+                _transitions
+                    .Where(x => x.FromId == question.Id)
+                    .Select(x => x.ToId);
+
+            return _items.Where(x => nextItemIds.Contains(x.Id)).Cast<PollAnswer>();
         }
 
-        public IEnumerable<PollItem> FindNextFor(PollAnswer answer)
+        public PollItem FindNextFor(PollAnswer answer)
         {
-            // check answer exists in items if not return null
-            // check find transition and return
-            return null;
+            if (answer == null)
+            {
+                throw new DomainException($"Answer should not be null!");
+            }
+
+
+            if (!_items.Any(x => x.Id == answer.Id))
+            {
+                throw new DomainException($"Answer (id: '{answer.Id}' not present in the Poll!");
+            }
+
+            var nextItemIds =
+                _transitions
+                    .Where(x => x.FromId == answer.Id)
+                    .Select(x => x.ToId);
+
+            return _items.Single(x => nextItemIds.Contains(x.Id));
+        }
+
+        private static void ValidateQuestionTransition(Guid id, IEnumerable<PollItem> items, IEnumerable<Transition> transitions)
+        {
+            var questionTransitions = transitions.Where(x => x.FromId == id);
+
+            if (!questionTransitions.Any())
+            {
+                throw new DomainException($"Question (id: '{id}' should have transition!");
+            }
+        }
+
+        private static void ValidateAnswerTransition(Guid id, IEnumerable<PollItem> items, IEnumerable<Transition> transitions)
+        {
+            var answerTransitions = transitions.Where(x => x.FromId == id);
+
+            if (!answerTransitions.Any())
+            {
+                throw new DomainException($"Answer (id: '{id}' should have transition!");
+            }
         }
     }
 }
