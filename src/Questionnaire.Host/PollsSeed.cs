0122fe5654256d31400662ebbcc7722fe3ad97bc
iivchenko <iivchenko@live.com>
2020-08-27 14:48:44 +0200

    Add back Coffee poll

--- src/Questionnaire.Host/PollsSeed.cs
+++ src/Questionnaire.Host/PollsSeed.cs
@@ -22,6 +22,7 @@ namespace Questionnaire.Host
                 if (count == 0)
                 {
                     await repository.Create(CreateDoughnutHuntPoll());
+                    await repository.Create(CreateCoffeePoll());
                 }
             }
         }
@@ -33,33 +34,33 @@ namespace Questionnaire.Host
                 (
                     "Doughnut decision helper",
                     "This is not a game! All people in the world needs the guidence on if they want or need a Doughnunt. This guide is about saving your life and preventing wars!",
-                    CreateQuestion
+                    WithQuestion
                     (
                         "Do I want a Doughnut?",
                         ref transitions,
-                        CreateAnswer
+                        WithAnswer
                         (
                             "Yes",
                             ref transitions,
-                            CreateQuestion
+                            WithQuestion
                             (
                                 "Do I deserve it?",
                                 ref transitions,
-                                CreateAnswer
+                                WithAnswer
                                 (
                                     "Yes",
                                     ref transitions,
-                                    CreateQuestion
+                                    WithQuestion
                                     (
                                         "Are you sure?",
                                         ref transitions,
-                                        CreateAnswer
+                                        WithAnswer
                                         (
                                             "Yes",
                                             ref transitions,
                                             "Get it."
                                         ),
-                                        CreateAnswer
+                                        WithAnswer
                                         (
                                             "No",
                                             ref transitions,
@@ -67,21 +68,21 @@ namespace Questionnaire.Host
                                         )
                                     )
                                 ),
-                                CreateAnswer
+                                WithAnswer
                                 (
                                     "No",
                                     ref transitions,
-                                    CreateQuestion
+                                    WithQuestion
                                     (
                                         "Is it a good doughnut?",
                                         ref transitions,
-                                        CreateAnswer
+                                        WithAnswer
                                         (
                                             "Yes",
                                             ref transitions,
                                             "What are you waiting for? Grab it now."
                                         ),
-                                        CreateAnswer
+                                        WithAnswer
                                         (
                                             "No",
                                             ref transitions,
@@ -91,11 +92,135 @@ namespace Questionnaire.Host
                                 )
                             )
                         ),
-                        CreateAnswer
+                        WithAnswer
                         (
                             "No",
                             ref transitions,
                             "Maybe you want an apple?"
+                        )
+                    ),
+                    ref transitions
+                );
+        }
+
+        private static Poll CreateCoffeePoll()
+        {
+            var transitions = new List<Transition>();
+            return CreatePoll
+                (
+                    "What about morning Coffee",
+                    "This simple guide will help you in the morning while your brain still sleep to choose a proper good coffee drink.",
+                    WithQuestion
+                    (
+                        "Good morning! What about strong coffee?",
+                        ref transitions,
+                        WithAnswer
+                        (
+                            "Lets have something less strong",
+                            ref transitions,
+                            WithQuestion
+                            (
+                                "Any milk?",
+                                ref transitions,
+                                WithAnswer
+                                (
+                                    "Of course",
+                                    ref transitions,
+                                    WithQuestion
+                                    (
+                                        "Or may be even more milk?",
+                                        ref transitions,
+                                        WithAnswer
+                                        (
+                                            "More is better",
+                                            ref transitions,
+                                            "For today my suggestion for you is Latte. Have a nice drink =)"
+                                        ),
+                                        WithAnswer
+                                        (
+                                            "No not today",
+                                            ref transitions,
+                                            "For today my suggestion for you is Cappuccino. Have a nice drink =)"
+                                        )
+                                    )
+                                ),
+                                WithAnswer
+                                (
+                                    "Am... no... definitely no",
+                                    ref transitions,
+                                    WithQuestion
+                                    (
+                                        "Hm... may be some water?",
+                                        ref transitions,
+                                        WithAnswer
+                                        (
+                                            "Ho... lets add some water",
+                                            ref transitions,
+                                             "For today my suggestion for you is Americano. Have a nice drink =)"
+                                        ),
+                                        WithAnswer
+                                        (
+                                            "Pff... no... definitely no",
+                                            ref transitions,
+                                            "For today my suggestion for you is Black Coffee. Have a nice drink =)"
+                                        )
+                                    )
+                                )
+                            )
+                        ),
+                        WithAnswer
+                        (
+                            "Oh yes please!",
+                            ref transitions,
+                            WithQuestion
+                            (
+                                "Some milk?",
+                                ref transitions,
+                                WithAnswer
+                                (
+                                    "Nope, just coffee",
+                                    ref transitions,
+                                    WithQuestion
+                                    (
+                                        "A bigger cup or smaller?",
+                                        ref transitions,
+                                        WithAnswer
+                                        (
+                                            "Just little",
+                                            ref transitions,
+                                            "For today my suggestion for you is Ristretto. Have a nice drink =)"
+                                        ),
+                                        WithAnswer
+                                        (
+                                            "Give me more",
+                                            ref transitions,
+                                            "For today my suggestion for you is Espresso. Have a nice drink =)"
+                                        )
+                                    )
+                                ),
+                                WithAnswer
+                                (
+                                    "Yeah why not",
+                                    ref transitions,
+                                    WithQuestion
+                                    (
+                                        "Or may be foam?",
+                                        ref transitions,
+                                        WithAnswer
+                                        (
+                                            "No no just milk",
+                                            ref transitions,
+                                            "For today my suggestion for you is Flat White. Have a nice drink =)"
+                                        ),
+                                        WithAnswer
+                                        (
+                                            "Hm.. why not",
+                                            ref transitions,
+                                            "For today my suggestion for you is Espresso Machiato. Have a nice drink =)"
+                                        )
+                                    )
+                                )
+                            )
                         )
                     ),
                     ref transitions
@@ -107,7 +232,7 @@ namespace Questionnaire.Host
             return new Poll(Guid.NewGuid(), name, description, root, transitions);
         }
 
-        private static Question CreateQuestion(string text, ref List<Transition> transitions, params Answer[] answers)
+        private static Question WithQuestion(string text, ref List<Transition> transitions, params Answer[] answers)
         {
             var question = new Question(Guid.NewGuid(), text);
 
@@ -119,7 +244,7 @@ namespace Questionnaire.Host
             return question;
         }
 
-        private static Answer CreateAnswer(string text, ref List<Transition> transitions, Question question)
+        private static Answer WithAnswer(string text, ref List<Transition> transitions, Question question)
         {
             var answer = new Answer(Guid.NewGuid(), text);
 
@@ -128,7 +253,7 @@ namespace Questionnaire.Host
             return answer;
         }
 
-        private static Answer CreateAnswer(string text, ref List<Transition> transitions, string endText)
+        private static Answer WithAnswer(string text, ref List<Transition> transitions, string endText)
         {
             var answer = new Answer(Guid.NewGuid(), text);
             var end = new End(Guid.NewGuid(), endText);
